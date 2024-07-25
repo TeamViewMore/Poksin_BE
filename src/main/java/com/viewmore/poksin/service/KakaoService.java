2939698cfa03d4a9885213acd3ec0a3b28bb609e
7beunseo <kimes0403@gmail.com>
2024-07-26 04:19:37 +0900

    [REFACTOR] isSignedUp 메서드 간소화 #21

--- src/main/java/com/viewmore/poksin/service/KakaoService.java
+++ src/main/java/com/viewmore/poksin/service/KakaoService.java
@@ -66,19 +66,16 @@ public class KakaoService {
         Boolean isExist = false;
 
         isExist = userRepository.existsByKakaoUserId(id);
-
         if (isExist) {
             throw new DuplicateKakaoIdException("이미 카카오 계정으로 로그인하였습니다.");
         }
 
         isExist = userRepository.existsByUsername(username);
-
         if (isExist) {
             throw new DuplicateUsernameException("중복된 아이디가 존재합니다.");
         }
 
         isExist = counselorRepository.existsByUsername(username);
-
         if (isExist) {
             throw new DuplicateUsernameException("중복된 아이디가 존재합니다.");
         }
@@ -100,40 +97,7 @@ public class KakaoService {
     }
 
     public UserEntity isSignedUp(String token) {
-        // Bearer 토큰 준비
-        String bearerToken = "Bearer " + token;
-
-        // HTTP 요청 헤더 설정
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        headers.set("Authorization", bearerToken);
-
-        // HTTP 엔티티 생성
-        HttpEntity<String> entity = new HttpEntity<>("", headers);
-
-        // RestTemplate을 사용하여 POST 요청 보내기
-        RestTemplate restTemplate = new RestTemplate();
-        String url = "https://kapi.kakao.com/v2/user/me";
-        System.out.println(entity);
-
-        ResponseEntity<String> response;
-        try {
-            response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            throw new RuntimeException("조회 시 문제 발생");
-        }
-
-        // 응답 JSON 파싱하여 id 추출
-        ObjectMapper objectMapper = new ObjectMapper();
-        Long id;
-        try {
-            JsonNode rootNode = objectMapper.readTree(response.getBody());
-            id = rootNode.path("id").asLong();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            throw new RuntimeException("응답 파싱 시 문제 발생");
-        }
+        Long id = getKakaoId(token);
 
         UserEntity user = userRepository.findByKakaoUserId(id)
                 .orElseThrow(() -> new UsernameNotFoundException("해당 카카오 id를 가진 사용자를 찾을 수 없습니다: " + String.valueOf(id)));
