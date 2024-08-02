9a8be67f86601fa9975a398e781cee516e7305d0
7beunseo <kimes0403@gmail.com>
2024-08-03 01:20:26 +0900

    [CHORE] Cors 모든 요청에서 허용 #53

--- src/main/java/com/viewmore/poksin/config/WebConfig.java
+++ src/main/java/com/viewmore/poksin/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://poksin.netlify.app")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
