7b6749a36b717c66fbfaceb0dd1d6a4b9057e22b
7beunseo <kimes0403@gmail.com>
2024-08-05 23:15:42 +0900

    [FIX] 상담사 로그인 안되는 문제 해결 #71

--- src/main/java/com/viewmore/poksin/jwt/LoginFilter.java
+++ src/main/java/com/viewmore/poksin/jwt/LoginFilter.java
@@ -7,8 +7,11 @@ import com.viewmore.poksin.dto.user.CustomUserDetails;
 import com.viewmore.poksin.dto.response.ErrorResponseDTO;
 import com.viewmore.poksin.dto.response.ResponseDTO;
 import com.viewmore.poksin.dto.user.LoginResponseDTO;
+import com.viewmore.poksin.entity.CounselorEntity;
+import com.viewmore.poksin.entity.MainUserEntity;
 import com.viewmore.poksin.entity.RefreshEntity;
 import com.viewmore.poksin.entity.UserEntity;
+import com.viewmore.poksin.repository.CounselorRepository;
 import com.viewmore.poksin.repository.RefreshRedisRepository;
 import com.viewmore.poksin.repository.UserRepository;
 import jakarta.servlet.FilterChain;
@@ -26,6 +29,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
@@ -34,6 +38,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     private final JWTUtil jwtUtil;
     private final RefreshRedisRepository refreshRedisRepository;
     private final UserRepository userRepository;
+    private final CounselorRepository counselorRepository;
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
@@ -61,25 +66,31 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         String accessToken = jwtUtil.createJwt("accessToken", username, role, 86400000L);
         String refreshToken = jwtUtil.createJwt("refreshToken", username, role, 86400000L);
 
-
         response.setHeader("accessToken", "Bearer " + accessToken);
         response.setHeader("refreshToken", "Bearer " + refreshToken);
 
         addRefreshEntity(refreshToken, username);
 
-        UserEntity user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("해당 사용자 이름을 가진 사용자를 찾을 수 없습니다: " + username));
+        UserEntity user = userRepository.findByUsername(username).orElse(null);
+        CounselorEntity counselor = null;
 
-        Long id = user.getId();
+        if (user == null) {
+            counselor = counselorRepository.findByUsername(username)
+                    .orElseThrow(() -> new UsernameNotFoundException("해당 사용자 이름을 가진 사용자를 찾을 수 없습니다: " + username));
+        }
+
+        Long id = (user != null) ? user.getId() : counselor.getId();
+
         LoginResponseDTO loginResponseDTO = new LoginResponseDTO(id);
-
         ResponseDTO responseDTO = new ResponseDTO<>(SuccessCode.SUCCESS_LOGIN, loginResponseDTO);
+
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonResponse = objectMapper.writeValueAsString(responseDTO);
         response.getWriter().write(jsonResponse);
     }
+
 
     private void addRefreshEntity(String refresh, String username) {
         RefreshEntity refreshEntity = new RefreshEntity(refresh, username);
